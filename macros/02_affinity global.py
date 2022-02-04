aa5e410d4351b96e7a8b2ddd821e87078231c7f6
Charles Dessenius <dessenius.charles@gmail.com>
2022-02-04 09:40:40 +0100

    Update macros

--- macros/02_affinity global.py
+++ macros/02_affinity global.py
@@ -6,20 +6,20 @@ app = {                             # REQUIRED dict, must be named 'app'
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x2B033F, 'Undo', [Keycode.CONTROL, 'z']),
-        (0x2B033F, 'Copy', [Keycode.CONTROL, 'c']),
+        (0x2B033F, 'Marquee', 'M'),
         (0x2B033F, 'Save as', [Keycode.CONTROL, 'S']),
         # 2nd row ----------
-        (0x2B033F, 'Redo', [Keycode.CONTROL, 'y']),
-        (0x2B033F, 'Paste', [Keycode.CONTROL, 'v']),
+        (0x2B033F, 'Fill', [Keycode.SHIFT, Keycode.F5]),
+        (0x2B033F, 'Deselect', [Keycode.CONTROL, 'd']),
         (0x2B033F, 'Export', [Keycode.CONTROL, Keycode.ALT, 'S']),
         # 3rd row ----------
         (0x2B033F, 'Shift', [Keycode.SHIFT]),
-        (0x2B033F, 'Box', 'M'),
+        (0x2B033F, 'Dupli', [Keycode.CONTROL, 'j']),
         (0x0081D5, 'Enter', [Keycode.ENTER]),
         # 4th row ----------
         (0x2B033F, 'Ctrl', [Keycode.CONTROL]),
         (0x2B033F, 'Alt', [Keycode.ALT]),
-        (0x2B033F, 'Close', [Keycode.CONTROL, 'w']),
+        (0x2B033F, 'Space', [Keycode.SPACE]),
         # Encoder button ---
         (0x000000, '', 'x')
     ]
--- macros/05_studio.py
+++ macros/05_studio.py
@@ -6,11 +6,11 @@ app = {                             # REQUIRED dict, must be named 'app'
         # COLOR    LABEL    KEY SEQUENCE
         # 1st row ----------
         (0x2B033F, 'Raw', [Keycode.CONTROL, 'A']),
-        (0x2B033F, 'Copy', [Keycode.CONTROL, 'c']),
+        (0x2B033F, 'Marquee', 'M'),
         (0x2B033F, 'Save as', [Keycode.CONTROL, 'S']),
         # 2nd row ----------
         (0x2B033F, 'Fill', [Keycode.SHIFT, Keycode.F5]),
-        (0x2B033F, 'Paste', [Keycode.CONTROL, 'v']),
+        (0x2B033F, 'Deselect', [Keycode.CONTROL, 'd']),
         (0x2B033F, 'Export', [Keycode.CONTROL, Keycode.ALT, 's']),
         # 3rd row ----------
         (0x2B033F, 'Shift', [Keycode.SHIFT]),
@@ -19,7 +19,7 @@ app = {                             # REQUIRED dict, must be named 'app'
         # 4th row ----------
         (0x2B033F, 'Ctrl', [Keycode.CONTROL]),
         (0x2B033F, 'Alt', [Keycode.ALT]),
-        (0x2B033F, 'Close', [Keycode.CONTROL, 'w']),
+        (0x2B033F, 'Space', [Keycode.SPACE]),
         # Encoder button ---
         (0x000000, '', 'x')
     ]
